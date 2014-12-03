f6b9839e6f6231ebb3855f62d3c74b9ebf79bf24
Pere Cullera <cameame@gmail.com>
2014-12-03 20:45:16 +0100

    first commit

---
+++ app/src/main/java/com/example/perecullera/webview4d/WebViewActivity.java
@@ -0,0 +1,49 @@
+package com.example.perecullera.webview4d;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+
+public class WebViewActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        Intent intent = getIntent();
+        String server = intent.getExtras().getString("server");
+        String port = intent.getExtras().getString("port");
+
+
+        String BaseUrl = "http://";
+        String fullurl = BaseUrl + server + ":" + port;
+        //
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl(fullurl);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.web_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
