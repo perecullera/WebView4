2f92393b9e4be0e2e71b15510add2727a6c8e909
Pere Cullera <cameame@gmail.com>
2015-01-22 16:51:38 +0100

    settings implemented with listener

--- app/src/main/java/com/example/perecullera/webview4d/MainActivity.java
+++ app/src/main/java/com/example/perecullera/webview4d/MainActivity.java
@@ -3,48 +3,104 @@ package com.example.perecullera.webview4d;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.net.URI;
+import java.util.Map;
 
-public class MainActivity extends ActionBarActivity implements
-        OnClickListener {
+import static java.lang.String.valueOf;
 
-    Button submit, exit;
-    String server, port;
-    EditText serverinput, portinput;
-    SharedPreferences sh_Pref;
-    SharedPreferences.Editor toEdit;
+
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
+
+    private DrawerLayout mDrawer;
+    private ListView mDrawerOptions;
+    private static final String[] values = {"Settings", "Refresh", "Drawer 3"};
+    WebView webView;
+    SharedPreferences sPref;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getInit();
+        setContentView(R.layout.activity_web_view);
 
+        //drawer
+        mDrawerOptions = (ListView) findViewById(R.id.left_drawer);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
+        mDrawerOptions.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values));
+        mDrawerOptions.setOnItemClickListener(this);
+
+
+
+        //Intent intent = getIntent();
+        sPref = PreferenceManager.getDefaultSharedPreferences(this);
+        String server = sPref.getString("Server", "DEFAULT");
+        String port = sPref.getString("Port", "DEFAULT");
+
+        //debug
+        System.out.println("preferencias "+ sPref.toString());
+        System.out.println("server "+ server);
+        System.out.println("port "+ port);
+        Toast.makeText(this, "preferencias" + sPref.toString(), Toast.LENGTH_SHORT).show();
+
+
+
+        String BaseUrl = "http://";
+        String fullurl = BaseUrl + server +":"+ port;
+        fullurl = valueOf(URI.create(fullurl));
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(fullurl);
+
+        SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                // Implementation
+                String server = prefs.getString("Server", "DEFAULT");
+                String port = prefs.getString("Port", "DEFAULT");
+
+
+                String BaseUrl = "http://";
+                String fullurl = BaseUrl + server +":"+ port;
+                fullurl = valueOf(URI.create(fullurl));
+                webView.loadUrl(fullurl);
+                Toast.makeText(getApplicationContext(), "listener main " + sPref.toString(), Toast.LENGTH_SHORT).show();
+                System.out.println("url " + fullurl);
+            }
+        };
+        sPref.registerOnSharedPreferenceChangeListener(listener);
+        // amaga actionBar
+        getSupportActionBar().hide();
+
+        //debug
+        Map<String,?> keys = sPref.getAll();
+
+        for(Map.Entry<String,?> entry : keys.entrySet()){
+            Log.d("map values", entry.getKey() + ": " +
+                    entry.getValue().toString());
+        }
     }
-    public void getInit() {
-
-        submit = (Button) findViewById(R.id.submit);
-        exit = (Button) findViewById(R.id.exit);
-        serverinput = (EditText) findViewById(R.id.serverinput);
-        portinput = (EditText) findViewById(R.id.portinput);
-        submit.setOnClickListener(this);
-        exit.setOnClickListener(this); }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-
+        getMenuInflater().inflate(R.menu.web_view, menu);
         return true;
     }
 
@@ -59,33 +115,17 @@ public class MainActivity extends ActionBarActivity implements
         }
         return super.onOptionsItemSelected(item);
     }
+
     @Override
-    public void onClick(View currentButton) {
-        switch (currentButton.getId()) {
-            case R.id.submit:
-                server = serverinput.getText().toString();
-                port = portinput.getText().toString();
-                sharedPreferences();
-                Toast.makeText(this, "Details are saved", 20).show();
-                Intent launchBrowser;
-                launchBrowser = new Intent(this, WebViewActivity.class);
-                launchBrowser.putExtra("server",server);
-                launchBrowser.putExtra("port", port);
-                //launchBrowser.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
-                startActivity(launchBrowser);
-                break;
-            case R.id.exit:
-                finish();
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this, "Pulsado " + values[i], Toast.LENGTH_SHORT).show();
+        if (values[i].equals("Settings")){
+            Intent settingsInt = new Intent(this, SettingsActivity.class);
+            settingsInt.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
+            startActivity(settingsInt);
+        }else if (values[i].equals("Refresh")){
+            webView.loadUrl(webView.getUrl());
         }
+        mDrawer.closeDrawers();
     }
-
-    private void sharedPreferences() {
-        sh_Pref = getSharedPreferences("Login Credentials", MODE_PRIVATE);
-        toEdit = sh_Pref.edit();
-        toEdit.putString("Server", server);
-        toEdit.putString("Port", port);
-        toEdit.commit();
-    }
-
-
 }
---
+++ app/src/main/java/com/example/perecullera/webview4d/SettingsActivity.java
@@ -0,0 +1,103 @@
+package com.example.perecullera.webview4d;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class SettingsActivity extends ActionBarActivity implements
+        OnClickListener {
+
+    Button submit, exit;
+    String server, port;
+    EditText serverinput, portinput;
+    SharedPreferences sh_Pref;
+    SharedPreferences.Editor toEdit;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getInit();
+
+        SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                // Implementation
+                System.out.println(key);
+                Toast.makeText(getApplicationContext(), "listener settings"+ prefs.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        };
+        sh_Pref = PreferenceManager.getDefaultSharedPreferences(this);
+        sh_Pref.registerOnSharedPreferenceChangeListener(listener);
+
+    }
+    public void getInit() {
+
+        submit = (Button) findViewById(R.id.submit);
+        exit = (Button) findViewById(R.id.exit);
+        serverinput = (EditText) findViewById(R.id.serverinput);
+        portinput = (EditText) findViewById(R.id.portinput);
+        submit.setOnClickListener(this);
+        exit.setOnClickListener(this); }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onClick(View currentButton) {
+        switch (currentButton.getId()) {
+            case R.id.submit:
+                server = serverinput.getText().toString();
+                port = portinput.getText().toString();
+                sharedPreferences();
+                Toast.makeText(this, "Details are saved", Toast.LENGTH_SHORT).show();
+                Intent launchBrowser;
+                launchBrowser = new Intent(this, MainActivity.class);
+//                launchBrowser.putExtra("server",server);
+//                launchBrowser.putExtra("port", port);
+                launchBrowser.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
+                startActivity(launchBrowser);
+                break;
+            case R.id.exit:
+                finish();
+        }
+    }
+
+    private void sharedPreferences() {
+
+        toEdit = sh_Pref.edit();
+        toEdit.putString("Server", server);
+        toEdit.putString("Port", port);
+        toEdit.commit();
+    }
+
+
+}
